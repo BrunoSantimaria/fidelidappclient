e45b9b571013eaeacbd87a0364289cb3effe141d
Bruno Santimaria <santimariabruno@gmail.com>
2024-09-30 13:34:36 -0300

    a

--- src/layaout/components/ContactInfo.tsx
+++ src/layaout/components/ContactInfo.tsx
@@ -3,6 +3,8 @@ import { Typography } from "@mui/material";
 import { contactInfo } from "../../data/contactInfo";
 
 export const ContactInfo = () => {
+  console.log("hola");
+
   return (
     <>
       <Typography sx={{ fontSize: "24px", marginBottom: "10px" }}>{contactInfo.title}</Typography>
