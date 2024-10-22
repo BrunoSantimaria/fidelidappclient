75b98b4beb0c8fc63c25cd742faada0f2af14226
Bruno Santimaria <santimariabruno@gmail.com>
2024-10-21 23:01:58 -0300

    fix qr

--- src/dashboard/pages/AccountQr/AccountQr.tsx
+++ src/dashboard/pages/AccountQr/AccountQr.tsx
@@ -4,9 +4,7 @@ import QRCode from "react-qr-code";
 import { useDashboard } from "../../../hooks";
 
 export const AccountQr = () => {
-  const { user, refreshAccount } = useAuthSlice();
-
-  const qrValue = user.accounts.accountQr;
+  const { user } = useAuthSlice();
 
   if (!user) return <div>No hay codigo qr en esta cuenta.</div>;
   return (
@@ -15,7 +13,7 @@ export const AccountQr = () => {
         <p className='text-center text-xl'>Usa este codigo QR para que tus clientes lo escaneen en cada visita</p>
       </div>
       <div className='flex justify-center'>
-        <QRCode value={qrValue} />
+        <QRCode value={user?.accounts.accountQr} />
       </div>
       <span className='bg-main text-white p-2 flex justify-center w-fit m-auto rounded-md cursor-pointer '>Regenerar codigo QR</span>
     </div>
