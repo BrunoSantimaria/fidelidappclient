633ae8e854ea5aadc862d7d6e9bc069ebe82e110
Bruno Santimaria <santimariabruno@gmail.com>
2024-10-21 23:05:04 -0300

    f

--- src/dashboard/pages/AccountQr/AccountQr.tsx
+++ src/dashboard/pages/AccountQr/AccountQr.tsx
@@ -1,21 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuthSlice } from "../../../hooks/useAuthSlice";
 import QRCode from "react-qr-code";
-import { useDashboard } from "../../../hooks";
 
 export const AccountQr = () => {
   const { user } = useAuthSlice();
+  const [loading, setLoading] = useState(true);
 
-  if (!user) return <div>No hay codigo qr en esta cuenta.</div>;
+  useEffect(() => {
+    if (user) {
+      setLoading(false);
+    }
+  }, [user]);
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
+  if (!user || !user.accounts || !user.accounts.accountQr) {
+    return <div>No hay código QR disponible en esta cuenta.</div>;
+  }
+
   return (
     <div className='mt-40 space-y-12'>
       <div className='flex justify-center'>
-        <p className='text-center text-xl'>Usa este codigo QR para que tus clientes lo escaneen en cada visita</p>
+        <p className='text-center text-xl'>Usa este código QR para que tus clientes lo escaneen en cada visita</p>
       </div>
       <div className='flex justify-center'>
-        <QRCode value={user?.accounts.accountQr} />
+        {/* Renderizar el código QR solo si está disponible */}
+        <QRCode value={user.accounts.accountQr} />
       </div>
-      <span className='bg-main text-white p-2 flex justify-center w-fit m-auto rounded-md cursor-pointer '>Regenerar codigo QR</span>
+      <span className='bg-main text-white p-2 flex justify-center w-fit m-auto rounded-md cursor-pointer'>Regenerar código QR</span>
     </div>
   );
 };
