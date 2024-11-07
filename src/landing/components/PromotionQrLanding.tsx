2693943a616969f7153cc6aeac9b8d73308257a0
Bruno Santimaria <santimariabruno@gmail.com>
2024-11-06 21:59:55 -0300

    asd

--- src/landing/components/PromotionQrLanding.tsx
+++ src/landing/components/PromotionQrLanding.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import qrcode from "../../assets/qr-code.png";
+import QRCode from "react-qr-code";
 export const PromotionQrLanding = () => {
   return (
     <div className='h-screen w-screen bg-gradient-to-br from-gray-300 to-main/30'>
@@ -7,7 +8,8 @@ export const PromotionQrLanding = () => {
         <span className='text-xl'>
           Escanea el QR desde la <span className='font-bold'>FidelidCard</span> que acaba de llegar a tu correo.
         </span>
-        <img src={qrcode} alt='QR Code' className='w-80 m-auto mt-8' />
+        <QRCode value='3a8a8b36-5ad7-447f-8316-aac8cf4ba63a' size={256} />
+
         <span className='mt-10'>Una vez escaneado exitosamente puedes cerrar esta ventana con el botón debajo.</span>
       </div>
     </div>
